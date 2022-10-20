646da3650dac4ad44b41838a652904f9024c61e0
Daisuke Ito <ito-daisuke-hv@ynu.jp>
2022-10-20 17:46:58 +0900

    Update  KosakaQbackend._check_run_input()
    Yuma, Yugo, please edit this func.

--- KosakaQbackend.py
+++ KosakaQbackend.py
@@ -18,3 +18,14 @@ class KosakaQbackend(BackendV2):
         if BACKEND == "rabi":
             self.IP = "192.168.11.185"  # サーバーのローカルIP
             self.version = 2
+    
+    def _check_run_input(run_input):
+        """Return the qobj to be run on the backends.
+
+        Args:
+            run_input: argument of KosakaQbackend.run()
+
+        Returns:
+            Qobj: the qobj to be run on the backends
+        """
+        pass  # 雄真・優悟、この行を消してを消して書き加えてください！
