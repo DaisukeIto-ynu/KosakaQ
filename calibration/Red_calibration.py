b365acd989f9de245d6f326f835be0c4d87290d9
Daisuke Ito <ito-daisuke-hv@ynu.jp>
2022-11-02 16:56:11 +0900

    update

--- calibration/Red_calibration.py
+++ calibration/Red_calibration.py
@@ -9,6 +9,7 @@ class Red_calibration():
     def __init__(self):
         self.mode = None
         self.job_num = 0
+        self.mode = []
     
     def run(self, mode):  # 大輔が作ります
         """
@@ -18,12 +19,20 @@ class Red_calibration():
         # self.result = []  # Rabi_project20_E6EL06_area06_NV04_PLE_all_0.txtの内容が入ったlistを返します。
         # self.power = []  #周波数 vs.laser_power
         self.job_num += 1  # 発行したjobの数
+        self.mode.append(mode)
         self.flag.append({})  # 各種Flag
         self.flag[-1]["get_result"] = False
         self.flag[-1]["calibration"] = False
         return self.job[-1]  # result[0]=frequencyのlist, result[1]=count（縦軸), result[2] = エラーバーのlist
     
-    
+    def jobs(self):
+        if self.job_num == 0:
+            print("There is no job.")
+        else:
+            for i in range(self.job_num):
+                print("job",i+1,"... ","mode:",self.mode[i], " get_result: not yet")
+             
+                
     def get_result(self, job_num = 0):  # job_num = 0にすることで、使うとき job_num-1 = -1 となり、最新のが使える。
         # self.flag[-1]["get_result"] = True　だったら、already executed表示
         # job_status確認して表示
@@ -31,7 +40,7 @@ class Red_calibration():
         # status:doneだったら/なったら、result取ってくる。
         # job_num > self.job_num or job_num < 0 or not( type(job_num) == int )　だったら、raiseする。
         # 最後に、self.flag[job_num-1]["get_result"] = True
-        pass
+        pass  # result[job_num-1][0]=frequencyのlist, result[job_num-1][1]=count（縦軸), result[job_num-1][2] = エラーバーのlist
     
     
     def draw(fitting=False, error=False, Ey=False, E1E2=False, save=False, job_num = 0):
